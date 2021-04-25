06cb0e470c2529dbe209efa9ab6c29296ffa5016
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-04-25 22:03:43 +0300

    Adding an object to where it belongs

---
+++ index.js
@@ -0,0 +1,14 @@
+let price, topping_price, crust_price;
+let total = 0;
+
+function Havepizza(name, size, crust, topping, total) {
+    this.name = name;
+    this.size = size;
+    this.crust = crust;
+    this.topping = topping;
+    this.total = total;
+}
+
+$(document).ready(function () {
+    
+})
