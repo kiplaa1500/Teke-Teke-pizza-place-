b54a6a2976d431cbff058a4530642f7ce158f5c4
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-04-25 23:08:06 +0300

    Adding pizza button

--- index.js
+++ index.js
@@ -79,7 +79,57 @@ $(document).ready(function () {
         let checkoutTotal = 0;
         checkoutTotal = checkoutTotal + total;
 
-        
+        $("#pizzaname").html($(".name option:selected").val());
+        $("#pizzasize").html($("#size option:selected").val());
+        $("#pizzacrust").html($("#crust option:selected").val());
+        $("#pizzatopping").html(ptopping.join(", "));
+        $("#totals").html(total);
 
-    })
-})
+        // Adding pizza button
+        $("button.addPizza").click(function () {
+            let pname = $(".name option:selected").val();
+            let psize = $("#size option:selected").val();
+            let pcrust = $("#crust option:selected").val();
+            let ptopping = [];
+            $.each($("input[name='toppings']:checked"), function () {
+                ptopping.push($(this).val());
+            });
+            console.log(ptopping.join(", "));
+            switch (psize) {
+                case "0":
+                    price = 0;
+                    break;
+                case "large":
+                    price = 1200;
+                    console.log(price);
+                    break;
+                case "medium":
+                    price = 850;
+                    console.log("The price is " + price);
+                    break;
+                case "small":
+                    price = 600;
+                    console.log(price);
+                default:
+                    console.log("error");
+            }
+            switch (pcrust) {
+                case "0":
+                    crust_price = 0;
+                    break;
+                case "Crispy":
+                    crust_price = 200;
+                    break;
+                case "Stuffed":
+                    crust_price = 150;
+                    break;
+                case "Gluten-free":
+                    crust_price = 180;
+                    break;
+                default:
+                    console.log("No price");
+            }
+            
+        event.preventDefault();
+    });
+});
