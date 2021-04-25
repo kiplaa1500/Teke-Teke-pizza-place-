b64e399f925daf4099096b0341e2f7a4262f1181
kiplaa1500 <dennis.kiplangat@student.moringaschool.com>
2021-04-25 23:11:58 +0300

    Adding order placement button

--- index.js
+++ index.js
@@ -168,6 +168,30 @@ $(document).ready(function () {
             $("#totalbill").append("Your bill plus delivery fee is: " + deliceryamount);
         });
 
-        // when one clicks place order button
+        // Placing order button
+        $("button#final-order").click(function (event) {
+            event.preventDefault();
+
+            $("#pizzatotal").hide();
+            $(".delivery").hide();
+            $("button#final-order").hide();
+            let deliceryamount = checkoutTotal + 150;
+            console.log("Final Bill is: " + deliceryamount);
+            let person = $("input#name").val();
+            let phone = $("input#phone").val();
+            let location = $("input#location").val();
+
+            if ($("input#name").val() && $("input#phone").val() && $("input#location").val() != "") {
+
+                $("#finallmessage").append(person + ", We have recieved your order and it will be delivered to you at " + location + ". Prepare sh. " + deliceryamount);
+                $("#totalbill").hide();
+                $("#finallmessage").slideDown(1200);
+            } else {
+                alert("Please fill in the details for delivery!");
+                $(".delivery").show();
+                $("button#final-order").show();
+            }
+        });
+        event.preventDefault();
     });
 });
